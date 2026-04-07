36e943e58a49eaf5a11b769aa63d6a93a911cb5c
fdgdgerg <fdgdgergdino@gmail.com>
2026-04-06 23:03:09 -0600

    Update init.lua

--- plugin/init.lua
+++ plugin/init.lua
@@ -7,7 +7,7 @@ local function init()
 	-- enable_sub_modules()
 	local opts = {
 		auto = true,
-		keywords = { "fdgdgerg", "resurrect", "wezterm" },
+		keywords = { "fdgdgerg", "resurrect", "wezterm", "Wezurrect" },
 	}
 	local plugin_path = dev.setup(opts)
 
